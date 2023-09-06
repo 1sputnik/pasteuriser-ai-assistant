e94f633b6c8a3ffe348517a9beee097f369f27d6
Lenonim <ilja.ist.am.bestem@mail.ru>
2023-09-06 19:54:28 +0300

    удалил data_changer

--- CppForecasting/data_changer/data_changer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "E:\work\pasteuriser-ai-assistant\CppForecasting\data_form\data_form.h"
-
-void parsing_data_per_cid(data_vector in_data, data_vector out_data, __int8 cid);
-
-void right_range(data_vector& data, int range);
