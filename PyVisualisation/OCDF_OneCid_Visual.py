8f60c6ea43f3a2b8ff3e117933343b3b67a359f9
Lenonim <ilja.ist.am.besten@mail.ru>
2024-02-18 12:48:30 +0300

    более информативные графики

--- PyVisualisation/OCDF_OneCid_Visual.py
+++ PyVisualisation/OCDF_OneCid_Visual.py
@@ -1,16 +1,18 @@
 import sys
 from matplotlib.pyplot import figure, show
 from ZoomPan import *
+from CidInfo import *
 
 
-def zoom_visualisation(all_x=[], all_y=[]):
+def zoom_visualisation(cid=str(), all_x=[], all_y=[]):
     fig = figure()
 
     ax = fig.add_subplot(111, xlim=(all_x[0], all_x[len(all_x) - 1]), ylim=(min(all_y), max(all_y)), autoscale_on=False)
 
     ax.plot(all_x, all_y, color='b')
-    ax.set_ylabel(f"Данные сида")
-    ax.set_xlabel("Дискретное время в секундах: t,sec")
+    ax.set_title(f"Данные сида {cid}")
+    ax.set_ylabel(f"{cids_info[int(cid)]}")
+    ax.set_xlabel("Дискретное время, сек")
     ax.grid(True)
 
     scale = 1.1
@@ -20,6 +22,7 @@ def zoom_visualisation(all_x=[], all_y=[]):
     show()
 
 # for data storage
+cid_ex = ""
 all_target_value = []
 all_time = []
 
@@ -34,9 +37,9 @@ work_file = open(file_name, 'r')
 for line in work_file.readlines():
     line = line.replace("\n", "")
     line_parts = line.split(';')
-    if int(float(line_parts[2]) > 0):
-        all_time.append(int(line_parts[1]))
-        all_target_value.append(float(line_parts[2]))
+    cid_ex = line_parts[0]
+    all_time.append(int(line_parts[1]))
+    all_target_value.append(float(line_parts[2]))
 work_file.close()
 
-zoom_visualisation(all_time, all_target_value)
+zoom_visualisation(cid_ex, all_time, all_target_value)
--- PyVisualisation/OCDF_SixCids_Visual.py
+++ PyVisualisation/OCDF_SixCids_Visual.py
@@ -1,27 +1,95 @@
 import sys
 from matplotlib.pyplot import figure, show
 from ZoomPan import *
+from CidInfo import *
 
 
 def zoom_visualisation(cid1_x=[], cid1_y=[], cid2_x=[], cid2_y=[], cid3_x=[], cid3_y=[], cid4_x=[], cid4_y=[], cid5_x=[], cid5_y=[], cid6_x=[], cid6_y=[]):
     fig = figure()
 
-    min_x = min(min(cid1_x), min(cid2_x), min(cid3_x), min(cid4_x), min(cid5_x), min(cid6_x))
-    max_x = max(max(cid1_x), max(cid2_x), max(cid3_x), max(cid4_x), max(cid5_x), max(cid6_x))
-    min_y = min(min(cid1_y), min(cid2_y), min(cid3_y), min(cid4_y), min(cid5_y), min(cid6_y))
-    max_y = max(max(cid1_y), max(cid2_y), max(cid3_y), max(cid4_y), max(cid5_y), max(cid6_y))
+    min_x_list = []
+    max_x_list = []
+    min_y_list = []
+    max_y_list = []
+    legend_list = []
+    ax_title = ""
+
+    if len(cid1_y) > 0:
+        ax_title += "1"
+        min_x_list.append(cid1_x[0])
+        max_x_list.append(cid1_x[len(cid1_x) - 1])
+        min_y_list.append(cid1_y[0])
+        max_y_list.append(cid1_y[len(cid1_y) - 1])
+        legend_list.append(cids_info[int(1)])
+    if len(cid2_y) > 0:
+        if len(ax_title) >= 1:
+            ax_title += " "
+        ax_title += "2"
+        min_x_list.append(cid2_x[0])
+        max_x_list.append(cid2_x[len(cid2_x) - 1])
+        min_y_list.append(cid2_y[0])
+        max_y_list.append(cid2_y[len(cid2_y) - 1])
+        legend_list.append(cids_info[int(2)])
+    if len(cid3_y) > 0:
+        if len(ax_title) >= 1:
+            ax_title += " "
+        ax_title += "3"
+        min_x_list.append(cid3_x[0])
+        max_x_list.append(cid3_x[len(cid3_x) - 1])
+        min_y_list.append(cid3_y[0])
+        max_y_list.append(cid3_y[len(cid3_y) - 1])
+        legend_list.append(cids_info[int(3)])
+    if len(cid4_y) > 0:
+        if len(ax_title) >= 1:
+            ax_title += " "
+        ax_title += "4"
+        min_x_list.append(cid4_x[0])
+        max_x_list.append(cid4_x[len(cid4_x) - 1])
+        min_y_list.append(cid4_y[0])
+        max_y_list.append(cid4_y[len(cid4_y) - 1])
+        legend_list.append(cids_info[int(4)])
+    if len(cid5_y) > 0:
+        if len(ax_title) >= 1:
+            ax_title += " "
+        ax_title += "5"
+        min_x_list.append(cid5_x[0])
+        max_x_list.append(cid5_x[len(cid5_x) - 1])
+        min_y_list.append(cid5_y[0])
+        max_y_list.append(cid5_y[len(cid5_y) - 1])
+        legend_list.append(cids_info[int(5)])
+    if len(cid6_y) > 0:
+        if len(ax_title) >= 1:
+            ax_title += " "
+        ax_title += "6"
+        min_x_list.append(cid6_x[0])
+        max_x_list.append(cid6_x[len(cid6_x) - 1])
+        min_y_list.append(cid6_y[0])
+        max_y_list.append(cid6_y[len(cid6_y) - 1])
+        legend_list.append(cids_info[int(6)])
+
+    min_x = min(min_x_list)
+    max_x = max(max_x_list)
+    min_y = min(min_y_list)
+    max_y = max(max_y_list)
 
     ax = fig.add_subplot(111, xlim=(min_x, max_x), ylim=(min_y, max_y), autoscale_on=False)
 
-    ax.plot(cid1_x, cid1_y, color='blue')
-    ax.plot(cid2_x, cid2_y, color='red')
-    ax.plot(cid3_x, cid3_y, color='green')
-    ax.plot(cid4_x, cid4_y, color='orange')
-    ax.plot(cid5_x, cid5_y, color='purple')
-    ax.plot(cid6_x, cid6_y, color='black')
-    ax.set_title(f"Данные временного ряда")
+    if len(cid1_y) > 0:
+        ax.plot(cid1_x, cid1_y, color='blue')
+    if len(cid2_y) > 0:
+        ax.plot(cid2_x, cid2_y, color='red')
+    if len(cid3_y) > 0:
+        ax.plot(cid3_x, cid3_y, color='green')
+    if len(cid4_y) > 0:
+        ax.plot(cid4_x, cid4_y, color='orange')
+    if len(cid5_y) > 0:
+        ax.plot(cid5_x, cid5_y, color='purple')
+    if len(cid6_y) > 0:
+        ax.plot(cid6_x, cid6_y, color='black')
+    ax.set_title(f"Данные сидов: {ax_title}")
     ax.set_xlabel("Данные сидов")
-    ax.set_xlabel("Дискретное время в секундах: t,sec")
+    ax.set_xlabel("Дискретное время, сек")
+    ax.legend(legend_list, loc="upper left")
     ax.grid(True)
 
     scale = 1.1
@@ -90,31 +158,5 @@ for i in range(len(all_time) - 1):
     elif all_data_cid[i] == 6:
         cid6_value.append(all_values[i])
         cid6_time.append(all_time[i])
-
-if len(cid1_time) == 0:
-    cid1_time.append(0)
-if len(cid2_time) == 0:
-    cid2_time.append(0)
-if len(cid3_time) == 0:
-    cid3_time.append(0)
-if len(cid4_time) == 0:
-    cid4_time.append(0)
-if len(cid5_time) == 0:
-    cid5_time.append(0)
-if len(cid6_time) == 0:
-    cid6_time.append(0)
-
-if len(cid1_value) == 0:
-    cid1_value.append(0)
-if len(cid2_value) == 0:
-    cid2_value.append(0)
-if len(cid3_value) == 0:
-    cid3_value.append(0)
-if len(cid4_value) == 0:
-    cid4_value.append(0)
-if len(cid5_value) == 0:
-    cid5_value.append(0)
-if len(cid6_value) == 0:
-    cid6_value.append(0)
 
 zoom_visualisation(cid1_time, cid1_value, cid2_time, cid2_value, cid3_time, cid3_value, cid4_time, cid4_value, cid5_time, cid5_value, cid6_time, cid6_value)
